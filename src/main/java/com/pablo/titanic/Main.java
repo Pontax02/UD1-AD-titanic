b5a452f3cfb210b697048c7827073a3fbf79122b
Pontax02 <ppontanilla07@gmail.com>
2025-09-28 18:51:46 +0200

    :sparkles: feat(Main): Print all the data

--- src/main/java/com/pablo/titanic/Main.java
+++ src/main/java/com/pablo/titanic/Main.java
@@ -1,13 +1,31 @@
 package com.pablo.titanic;
 
-/**
- * Hello world!
- */
+import com.pablo.titanic.model.Persona;
+import com.pablo.titanic.repository.PersonaRepositoryImpl;
+import com.pablo.titanic.service.PersonaService;
+import com.pablo.titanic.service.PersonaServiceImpl;
+import com.pablo.titanic.storage.PersonasStorageImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+        //Creamos el service
+        PersonaService service = new PersonaServiceImpl(new PersonasStorageImpl(),new PersonaRepositoryImpl());
 
+        System.out.println("Mostrando datos de la tripulación del Titanic");
 
+        service.importFromFile("res/titanic.csv");
 
+        List<Persona> personas = service.getAll();
+
+        System.out.println();
+        System.out.println("Mostrando todas las personas");
+        for (Persona persona : personas) {
+            System.out.println(persona);
+        }
 
 
     }
